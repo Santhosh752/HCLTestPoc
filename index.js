ee4a91dbbc692492531d128d7213e80eeae29840
santhosh <darshansanthu@gmail.com>
2026-03-02 22:06:09 +0530

    docker file

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Demo App is running in AKS!");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
